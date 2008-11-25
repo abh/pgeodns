36d9759017d370c2848ec08db1bf2625bbc84b40
Ask Bjørn Hansen <ask@develooper.com>
2008-11-25 22:34:12 +0000

    add example crontab
    git-svn-id: https://svn.develooper.com/projects/pgeodns/trunk@713 e01dd7d5-6be1-0310-aa60-dbbdb055a346

--- sync_config.sh
+++ sync_config.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 rsync --delete --exclude .svn --exclude *sample -a --port 7873 ddns1.develooper.com::dinamed-conf/ config/dist/
 
+
+# run this from crontab with something like 
+#   */3 * * * * sleep $(expr $RANDOM \% 60); cd /var/services/pgeodns/pgeodns; ./sync_config.sh 
+# 
